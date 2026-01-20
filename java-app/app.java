143fe18f225c99d7a8d2ae21f7f967d4ca517a56
patelajay01 <pa34666666@gmail.com>
2026-01-20 10:44:17 +0530

    Initial commit: Java, Node, Python Docker apps

---
+++ java-app/app.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java Docker Image Running!");
+        try {
+            Thread.sleep(1000000);
+        } catch (Exception e) {}
+    }
+}
